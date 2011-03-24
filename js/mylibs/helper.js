945fe3f0dd47904a6aa2e9aedeb9b71d105bdb18
Paul Irish <paul.irish@gmail.com>
2011-03-24 11:09:34 -0700

    tabs to spaces.

--- js/mylibs/helper.js
+++ js/mylibs/helper.js
@@ -9,9 +9,9 @@ window.MBP = window.MBP || {};
 // http://remysharp.com/2010/08/05/doing-it-right-skipping-the-iphone-url-bar/
 
 MBP.hideUrlBar = function () {
-	/mobile/i.test(navigator.userAgent) && !pageYOffset && !location.hash && setTimeout(function () {
-	window.scrollTo(0, 1);
-	}, 1000);
+    /mobile/i.test(navigator.userAgent) && !pageYOffset && !location.hash && setTimeout(function () {
+    window.scrollTo(0, 1);
+    }, 1000);
 }
 
 
@@ -21,13 +21,13 @@ MBP.hideUrlBar = function () {
 MBP.fastButton = function (element, handler) {
     this.element = element;
     this.handler = handler;
-	element.addEventListener('touchstart', this, false);
-	element.addEventListener('click', this, false);
+    element.addEventListener('touchstart', this, false);
+    element.addEventListener('click', this, false);
 };
 
 MBP.fastButton.prototype.handleEvent = function(event) {
     switch (event.type) {
-		case 'touchstart': this.onTouchStart(event); break;
+        case 'touchstart': this.onTouchStart(event); break;
         case 'touchmove': this.onTouchMove(event); break;
         case 'touchend': this.onClick(event); break;
         case 'click': this.onClick(event); break;
@@ -37,7 +37,7 @@ MBP.fastButton.prototype.handleEvent = function(event) {
 MBP.fastButton.prototype.onTouchStart = function(event) {
     event.stopPropagation();
     this.element.addEventListener('touchend', this, false);
-	document.body.addEventListener('touchmove', this, false);
+    document.body.addEventListener('touchmove', this, false);
     this.startX = event.touches[0].clientX;
     this.startY = event.touches[0].clientY;
     this.element.style.backgroundColor = "rgba(0,0,0,.7)";
@@ -45,7 +45,7 @@ MBP.fastButton.prototype.onTouchStart = function(event) {
 
 MBP.fastButton.prototype.onTouchMove = function(event) {
     if(Math.abs(event.touches[0].clientX - this.startX) > 10 || Math.abs(event.touches[0].clientY - this.startY) > 10) {
-		this.reset();
+        this.reset();
     }
 };
 MBP.fastButton.prototype.onClick = function(event) {
@@ -53,7 +53,7 @@ MBP.fastButton.prototype.onClick = function(event) {
     this.reset();
     this.handler(event);
     if(event.type == 'touchend') {
-		MBP.preventGhostClick(this.startX, this.startY);
+        MBP.preventGhostClick(this.startX, this.startY);
     }
     this.element.style.backgroundColor = "";
 };
@@ -65,16 +65,16 @@ MBP.fastButton.prototype.reset = function() {
 MBP.preventGhostClick = function (x, y) {
     MBP.coords.push(x, y);
     window.setTimeout(function (){
-    	MBP.coords.splice(0, 2);
+        MBP.coords.splice(0, 2);
     }, 2500);
 };
 ;
 MBP.ghostClickHandler = function (event) {
-	for(var i = 0, len = MBP.coords.length; i < len; i += 2) {
-		var x = MBP.coords[i];
+    for(var i = 0, len = MBP.coords.length; i < len; i += 2) {
+        var x = MBP.coords[i];
         var y = MBP.coords[i + 1];
         if(Math.abs(event.clientX - x) < 25 && Math.abs(event.clientY - y) < 25) {
-			event.stopPropagation();
+            event.stopPropagation();
             event.preventDefault();
         }
     }
@@ -96,21 +96,21 @@ MBP.splash = function () {
 // http://googlecode.blogspot.com/2009/07/gmail-for-mobile-html5-series.html
 
 MBP.autogrow = function (element, lh) {
-	var setLineHeight = (lh) ? 11 : 12;
-	var textLineHeight = element.style.lineHeight;
-	if (textLineHeight.indexOf("px") == -1) {
-		textLineHeight = setLineHeight;
-	} else {
-		textLineHeight = parseInt(textLineHeight, 10);
-	}
-	element.style.overflow = "hidden";
-	element.onkeyup = function(e){
-		var newHeight = this.scrollHeight;
-		var currentHeight = this.clientHeight;
-		if (newHeight > currentHeight) {
-			this.style.height = newHeight + 3 * textLineHeight + "px";
-		}
-	}
+    var setLineHeight = (lh) ? 11 : 12;
+    var textLineHeight = element.style.lineHeight;
+    if (textLineHeight.indexOf("px") == -1) {
+        textLineHeight = setLineHeight;
+    } else {
+        textLineHeight = parseInt(textLineHeight, 10);
+    }
+    element.style.overflow = "hidden";
+    element.onkeyup = function(e){
+        var newHeight = this.scrollHeight;
+        var currentHeight = this.clientHeight;
+        if (newHeight > currentHeight) {
+            this.style.height = newHeight + 3 * textLineHeight + "px";
+        }
+    }
 }
 
 
