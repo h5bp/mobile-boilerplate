04cc4480ab1701c063729cb2bcd6db01605ade3b
Paul Irish <paul.irish@gmail.com>
2011-04-04 22:42:22 -0700

    cleanup of scaleFix() to be a little smarter and DRYer.

--- js/mylibs/helper.js
+++ js/mylibs/helper.js
@@ -8,35 +8,26 @@ window.MBP = window.MBP || {};
 // Fix for iPhone viewport scale bug 
 // http://www.blog.highub.com/mobile-2/a-fix-for-iphone-viewport-scale-bug/
 
+MBP.viewportmeta = document.querySelector && document.querySelector('meta[name="viewport"]');
+MBP.ua = navigator.userAgent;
+
 MBP.scaleFix = function () {
-  var i;
-  var metas = document.getElementsByTagName('meta');
-    if (navigator.userAgent.match(/iPhone/i) && !navigator.userAgent.match(/Opera/i)) {
-      for (i=0; i<metas.length; i++) {
-        if (metas[i].name == "viewport") {
-          metas[i].content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
-        }
-      }
-      document.addEventListener("gesturestart", MBP.gestureStart, false);
-    }
+  if (MBP.viewportmeta && /iPhone|iPad/.test(MBP.ua) && !/Opera Mini/.test(MBP.ua)) {
+    MBP.viewportmeta.content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
+    document.addEventListener("gesturestart", MBP.gestureStart, false);
+  }
+};
+MBP.gestureStart = function () {
+    MBP.viewportmeta.content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
 };
 
-MBP.gestureStart = function () {
-    var i;
-    var metas = document.getElementsByTagName('meta');
-    for (i=0; i<metas.length; i++) {
-        if (metas[i].name == "viewport") {
-          metas[i].content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
-        }
-    }
-}
 
 // Hide URL Bar for iOS
 // http://remysharp.com/2010/08/05/doing-it-right-skipping-the-iphone-url-bar/
 
 MBP.hideUrlBar = function () {
-    /iPhone/i.test(navigator.userAgent) && !pageYOffset && !location.hash && setTimeout(function () {
-    window.scrollTo(0, 1);
+    /iPhone/.test(MBP.ua) && !pageYOffset && !location.hash && setTimeout(function () {
+      window.scrollTo(0, 1);
     }, 1000);
 };
 
