e57a683e2b16e58ea63f20a64b98d28fdb12c3f9
Shi Chuan <shichuanr@gmail.com>
2011-09-23 05:57:42 +0800

    tagging for v2

--- js/mylibs/helper.js
+++ js/mylibs/helper.js
@@ -1,16 +1,7 @@
-/*
- * MBP - Mobile boilerplate helper functions
- */
 (function(document){
-
 window.MBP = window.MBP || {}; 
-
-// Fix for iPhone viewport scale bug 
-// http://www.blog.highub.com/mobile-2/a-fix-for-iphone-viewport-scale-bug/
-
 MBP.viewportmeta = document.querySelector && document.querySelector('meta[name="viewport"]');
 MBP.ua = navigator.userAgent;
-
 MBP.scaleFix = function () {
   if (MBP.viewportmeta && /iPhone|iPad/.test(MBP.ua) && !/Opera Mini/.test(MBP.ua)) {
     MBP.viewportmeta.content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
@@ -21,22 +12,15 @@ MBP.gestureStart = function () {
     MBP.viewportmeta.content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
 };
 
-
-// Hide URL Bar for iOS and Android by Scott Jehl
-// https://gist.github.com/1183357
-
 MBP.hideUrlBar = function () {
 	var win = window,
 		doc = win.document;
-
-	// If there's a hash, or addEventListener is undefined, stop here
 	if( !location.hash || !win.addEventListener ){
 
-		//scroll to 1
 		window.scrollTo( 0, 1 );
 		var scrollTop = 1,
 
-		//reset to 0 on bodyready, if needed
+
 		bodycheck = setInterval(function(){
 			if( doc.body ){
 				clearInterval( bodycheck );
@@ -47,16 +31,12 @@ MBP.hideUrlBar = function () {
 
 		win.addEventListener( "load", function(){
 			setTimeout(function(){
-				//reset to hide addr bar at onload
 				win.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
 			}, 0);
 		}, false );
 	}
 };
 
-
-// Fast Buttons - read wiki below before using
-// https://github.com/shichuan/mobile-html5-boilerplate/wiki/JavaScript-Helper
 
 MBP.fastButton = function (element, handler) {
     this.element = element;
@@ -133,17 +113,11 @@ if (document.addEventListener) {
 MBP.coords = [];
 
 
-// iOS Startup Image
-// https://github.com/shichuan/mobile-html5-boilerplate/issues#issue/2
-
 MBP.splash = function () {
     var filename = navigator.platform === 'iPad' ? 'h/' : 'l/';
     document.write('<link rel="apple-touch-startup-image" href="/img/' + filename + 'splash.png" />' );
 };
 
-
-// Autogrow
-// http://googlecode.blogspot.com/2009/07/gmail-for-mobile-html5-series.html
 
 MBP.autogrow = function (element, lh) {
 
