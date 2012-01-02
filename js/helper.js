e1e174da7396bddbd9e6a9b52fc67d7f4b1b9ff6
Alex Kessinger <alex@mixedmedialabs.com>
2012-01-02 11:52:43 -0800

    Making hide url bar more usefull
    Split url bar into an intial function, and a general use function
    cached the scrollTop so that only needs to be detected once

--- js/helper.js
+++ js/helper.js
@@ -28,41 +28,63 @@ MBP.gestureStart = function () {
   * MIT License
 */
 
-MBP.hideUrlBar = function () {
+// If we split this up into two functions we can reuse
+// this function if we aren't doing full page reloads.
+
+// If we cache this we don't need to re-calibrate everytime we call
+// the hide url bar
+MBP.BODY_SCROLL_TOP = false;
+
+// So we don't redefine this function everytime we
+// we call hideUrlBar
+MBP.getScrollTop = function(){
+  var win = window,
+      doc = document;
+
+  return win.pageYOffset || doc.compatMode === "CSS1Compat" && doc.documentElement.scrollTop || doc.body.scrollTop || 0;
+};
+
+// It should be up to the mobile
+MBP.hideUrlBar = function(){
+    var win = window;
+
+    // if there is a hash, or MBP.BODY_SCROLL_TOP hasn't been set yet, wait till that happens
+    if( !location.hash && MBP.BODY_SCROLL_TOP !== false){
+        win.scrollTo( 0, MBP.BODY_SCROLL_TOP === 1 ? 0 : 1 );
+    }
+};
+
+MBP.hideUrlBarOnLoad = function () {
   var win = window,
       doc = win.document;
-	
+
   // If there's a hash, or addEventListener is undefined, stop here
   if( !location.hash && win.addEventListener ) {
-		
+
     //scroll to 1
     window.scrollTo( 0, 1 );
-    var scrollTop = 1,
-        getScrollTop = function() {
-          return win.pageYOffset || doc.compatMode === "CSS1Compat" && doc.documentElement.scrollTop || doc.body.scrollTop || 0;
-        },
-		
-        //reset to 0 on bodyready, if needed
-        bodycheck = setInterval(function() {
-          if( doc.body ) {
-            clearInterval( bodycheck );
-            scrollTop = getScrollTop();
-            win.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
-          }	
-        }, 15 );
-		
+    MBP.BODY_SCROLL_TOP = 1;
+
+    //reset to 0 on bodyready, if needed
+    bodycheck = setInterval(function() {
+      if( doc.body ) {
+        clearInterval( bodycheck );
+        MBP.BODY_SCROLL_TOP = MBP.getScrollTop();
+        MBP.hideUrlBar();
+      }
+    }, 15 );
+
     win.addEventListener( "load", function() {
       setTimeout(function() {
         //at load, if user hasn't scrolled more than 20 or so...
-        if( getScrollTop() < 20 ) {
+        if( MBP.getScrollTop() < 20 ) {
           //reset to hide addr bar at onload
-          win.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
+          MBP.hideUrlBar();
         }
       }, 0);
     } );
   }
-}
-
+};
 
 // Fast Buttons - read wiki below before using
 // https://github.com/h5bp/mobile-boilerplate/wiki/JavaScript-Helper
