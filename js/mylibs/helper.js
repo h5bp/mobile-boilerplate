71e068f1db8e417455313544791a3669e7372aa4
Shi Chuan <shichuanr@gmail.com>
2012-01-30 10:58:33 +0800

    fixing some indentation issue

--- js/mylibs/helper.js
+++ js/mylibs/helper.js
@@ -32,12 +32,12 @@ MBP.hideUrlBar = function () {
   var win = window,
       doc = win.document;
 	
-	// If there's a hash, or addEventListener is undefined, stop here
-	if( !location.hash && win.addEventListener ) {
+  // If there's a hash, or addEventListener is undefined, stop here
+  if( !location.hash && win.addEventListener ) {
 		
-		//scroll to 1
-		window.scrollTo( 0, 1 );
-		var scrollTop = 1,
+    //scroll to 1
+    window.scrollTo( 0, 1 );
+    var scrollTop = 1,
         getScrollTop = function() {
           return win.pageYOffset || doc.compatMode === "CSS1Compat" && doc.documentElement.scrollTop || doc.body.scrollTop || 0;
         },
@@ -51,18 +51,17 @@ MBP.hideUrlBar = function () {
           }	
         }, 15 );
 		
-		win.addEventListener( "load", function() {
-			setTimeout(function() {
-				//at load, if user hasn't scrolled more than 20 or so...
-				if( getScrollTop() < 20 ) {
-					//reset to hide addr bar at onload
-					win.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
-				}
-			}, 0);
-		} );
-	}
+    win.addEventListener( "load", function() {
+      setTimeout(function() {
+        //at load, if user hasn't scrolled more than 20 or so...
+        if( getScrollTop() < 20 ) {
+          //reset to hide addr bar at onload
+          win.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
+        }
+      }, 0);
+    } );
+  }
 }
-
 
 
 // Fast Buttons - read wiki below before using
