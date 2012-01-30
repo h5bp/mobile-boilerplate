41730b33af23cf3382a905064b801b6229eec45a
Divya Manian <divya.manian@gmail.com>
2012-01-29 19:01:03 -0800

    updating the build script to match the new one

--- build/createproject.sh
+++ build/createproject.sh
@@ -6,11 +6,25 @@
 
 ##first run
 # $ cd  html5-boilerplate/build
-# $ chmod +x createproject.sh && ./createproject.sh
+# $ chmod +x createproject.sh && ./createproject.sh [new_project]
 
 ##usage
 # $ cd  html5-boilerplate/build
-# $ ./createproject.sh
+# $ ./createproject.sh [new_project]
+
+#
+# If [new_project] is not specified the user we will prompted to enter it.
+#
+# The format of [new_project] should ideally be lowercase letters with no
+# spaces as it represents the directory name that your new project will live
+# in.
+#
+# If the new project is specified as just a name ( "foo" ) then the path
+# will be a sibling to html5-boilerplate's directory.
+#
+# If the new project is specified with an absolute path ( "/home/user/foo" )
+# that path will be used.
+#
 
 # find project root (also ensure script is ran from within repo)
 src=$(git rev-parse --show-toplevel) || {
@@ -23,27 +37,40 @@ src=$(git rev-parse --show-toplevel) || {
   exit 1
 }
 
-# get a name for new project
+if [ $# -eq 1 ]
+then
+    # get a name for new project from command line arguments
+    name="$1"
+fi
+
+# get a name for new project from input
 while [[ -z $name ]]
 do
     echo "To create a new html5-boilerplate project, enter a new directory name:"
     read name || exit
 done
-dst=$src/../$name
+
+if [[ "$name" = /* ]]
+then
+    dst=$name
+else
+    dst=$src/../$name
+fi
 
 if [[ -d $dst ]]
 then
     echo "$dst exists"
 else
     #create new project
-    mkdir -- "$dst" || exit 1
+    mkdir -p -- "$dst" || exit 1
 
-    #sucess message
+    #success message
     echo "Created Directory: $dst"
 
     cd -- "$src"
-    cp -vr -- css js img build test *.html *.xml *.txt *.png *.ico .htaccess *.appcache "$dst"
+    cp -vr -- css js img build *.html *.xml *.txt *.png *.ico .htaccess "$dst"
 
-    #sucess message
+    #success message
     echo "Created Project: $dst"
 fi
+
