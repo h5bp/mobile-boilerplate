1f0815f7adb03e9ae9e62b23f31f6f5daee7bdcf
Shi Chuan <shichuanr@gmail.com>
2012-01-30 10:48:29 +0800

    assign namespace to hide url bar function in helper.js

--- js/mylibs/helper.js
+++ js/mylibs/helper.js
@@ -27,39 +27,42 @@ MBP.gestureStart = function () {
   * (c) Scott Jehl, scottjehl.com
   * MIT License
 */
-(function( win ){
-	var doc = win.document;
+
+MBP.hideUrlBar = function () {
+  var win = window,
+      doc = win.document;
 	
 	// If there's a hash, or addEventListener is undefined, stop here
-	if( !location.hash && win.addEventListener ){
+	if( !location.hash && win.addEventListener ) {
 		
 		//scroll to 1
 		window.scrollTo( 0, 1 );
 		var scrollTop = 1,
-			getScrollTop = function(){
-				return win.pageYOffset || doc.compatMode === "CSS1Compat" && doc.documentElement.scrollTop || doc.body.scrollTop || 0;
-			},
+        getScrollTop = function() {
+          return win.pageYOffset || doc.compatMode === "CSS1Compat" && doc.documentElement.scrollTop || doc.body.scrollTop || 0;
+        },
 		
-			//reset to 0 on bodyready, if needed
-			bodycheck = setInterval(function(){
-				if( doc.body ){
-					clearInterval( bodycheck );
-					scrollTop = getScrollTop();
-					win.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
-				}	
-			}, 15 );
+        //reset to 0 on bodyready, if needed
+        bodycheck = setInterval(function() {
+          if( doc.body ) {
+            clearInterval( bodycheck );
+            scrollTop = getScrollTop();
+            win.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
+          }	
+        }, 15 );
 		
-		win.addEventListener( "load", function(){
-			setTimeout(function(){
+		win.addEventListener( "load", function() {
+			setTimeout(function() {
 				//at load, if user hasn't scrolled more than 20 or so...
-				if( getScrollTop() < 20 ){
+				if( getScrollTop() < 20 ) {
 					//reset to hide addr bar at onload
 					win.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
 				}
 			}, 0);
 		} );
 	}
-})( this );
+}
+
 
 
 // Fast Buttons - read wiki below before using
