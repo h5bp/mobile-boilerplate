2dbc8d539f4ebf005c5a97d4a1b246c0176820a6
Niclas Gelin <info@niclasgelin.se>
2012-04-08 19:53:08 +0200

    Consistent spaces and new lines
    Also converted had tabs to soft ones, since most lines had 'em

--- js/plugins.js
+++ js/plugins.js
@@ -1,4 +1,3 @@
-
 // usage: log('inside coolFunc', this, arguments);
 // paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
 window.log = function(){
@@ -14,7 +13,6 @@ window.log = function(){
 // make it safe to use console.log always
 (function(b){function c(){}for(var d="assert,clear,count,debug,dir,dirxml,error,exception,firebug,group,groupCollapsed,groupEnd,info,log,memoryProfile,memoryProfileEnd,profile,profileEnd,table,time,timeEnd,timeStamp,trace,warn".split(","),a;a=d.pop();){b[a]=b[a]||c}})((function(){try
 {console.log();return window.console;}catch(err){return window.console={};}})());
-
 
 // place any jQuery/helper plugins in here, instead of separate, slower script files.
 
