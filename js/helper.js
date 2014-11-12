986875a733617ce7e4b239ac204729afc58ae5c7
Nick Baugh <niftylettuce@gmail.com>
2014-11-12 12:16:26 -0500

    Fix JSHint errors

--- js/helper.js
+++ js/helper.js
@@ -6,6 +6,8 @@
 
     window.MBP = window.MBP || {};
 
+    /*globals MBP*/
+
     /**
      * Fix for iPhone viewport scale bug
      * http://www.blog.highub.com/mobile-2/a-fix-for-iphone-viewport-scale-bug/
@@ -148,7 +150,7 @@
         }
         this.reset(event);
         this.handler.apply(event.currentTarget, [event]);
-        if (event.type == 'touchend') {
+        if (event.type === 'touchend') {
             MBP.preventGhostClick(this.startX, this.startY);
         }
         var pattern = new RegExp(' ?' + this.pressedClass, 'gi');
@@ -201,7 +203,7 @@
     // This bug only affects touch Android 2.3 devices, but a simple ontouchstart test creates a false positive on
     // some Blackberry devices. https://github.com/Modernizr/Modernizr/issues/372
     // The browser sniffing is to avoid the Blackberry case. Bah
-    MBP.dodgyAndroid = ('ontouchstart' in window) && (navigator.userAgent.indexOf('Android 2.3') != -1);
+    MBP.dodgyAndroid = ('ontouchstart' in window) && (navigator.userAgent.indexOf('Android 2.3') !== -1);
 
     MBP.listenForGhostClicks = (function() {
         var alreadyRan = false;
@@ -232,7 +234,7 @@
             try {
                 el.addEventListener(evt, fn, bubble);
             } catch(e) {
-                if (typeof fn == 'object' && fn.handleEvent) {
+                if (typeof fn === 'object' && fn.handleEvent) {
                     el.addEventListener(evt, function(e){
                         // Bind fn as this and set first arg as event object
                         fn.handleEvent.call(fn,e);
@@ -243,7 +245,7 @@
             }
         } else if ('attachEvent' in el) {
             // check if the callback is an object and contains handleEvent
-            if (typeof fn == 'object' && fn.handleEvent) {
+            if (typeof fn === 'object' && fn.handleEvent) {
                 el.attachEvent('on' + evt, function(){
                     // Bind fn as this
                     fn.handleEvent.call(fn);
@@ -260,7 +262,7 @@
             try {
                 el.removeEventListener(evt, fn, bubble);
             } catch(e) {
-                if (typeof fn == 'object' && fn.handleEvent) {
+                if (typeof fn === 'object' && fn.handleEvent) {
                     el.removeEventListener(evt, function(e){
                         // Bind fn as this and set first arg as event object
                         fn.handleEvent.call(fn,e);
@@ -271,7 +273,7 @@
             }
         } else if ('detachEvent' in el) {
             // check if the callback is an object and contains handleEvent
-            if (typeof fn == 'object' && fn.handleEvent) {
+            if (typeof fn === 'object' && fn.handleEvent) {
                 el.detachEvent("on" + evt, function() {
                     // Bind fn as this
                     fn.handleEvent.call(fn);
@@ -288,7 +290,7 @@
      */
 
     MBP.autogrow = function(element, lh) {
-        function handler(e) {
+        function handler() {
             var newHeight = this.scrollHeight;
             var currentHeight = this.clientHeight;
             if (newHeight > currentHeight) {
@@ -299,10 +301,14 @@
         var setLineHeight = (lh) ? lh : 12;
         var textLineHeight = element.currentStyle ? element.currentStyle.lineHeight : getComputedStyle(element, null).lineHeight;
 
-        textLineHeight = (textLineHeight.indexOf('px') == -1) ? setLineHeight : parseInt(textLineHeight, 10);
+        textLineHeight = (textLineHeight.indexOf('px') === -1) ? setLineHeight : parseInt(textLineHeight, 10);
 
         element.style.overflow = 'hidden';
-        element.addEventListener ? element.addEventListener('input', handler, false) : element.attachEvent('onpropertychange', handler);
+
+        if (element.addEventListener)
+          element.addEventListener('input', handler, false);
+        else
+          element.attachEvent('onpropertychange', handler);
     };
 
     /**
